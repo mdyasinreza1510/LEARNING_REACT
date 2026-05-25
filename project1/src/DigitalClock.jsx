be6be1a695700513ea37df7bd1b9d2e133beb948
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-05-25 23:45:28 +0530

    created a digital clock

--- project1/src/DigitalClock.jsx
+++ project1/src/DigitalClock.jsx
@@ -3,11 +3,33 @@ import React, {useState, useEffect} from 'react'
 
 function DigitalClock(){
 
+const [time, settime]=useState(new Date());
+useEffect(()=>{
+const interval_id=setInterval(()=>{
+    settime(new Date());
+}, 1000);
+
+return ()=>{
+    clearInterval(interval_id)
+}
+}, []);
+
+function formattime(){
+    let hour=time.getHours();
+    const min=time.getMinutes();
+    const sec=time.getSeconds();
+    const merd= hour >=12 ? "PM" : "AM";
+    hour= hour % 12 || 12;//if hour =0 then it remainsame as 12
+    return `${hour}:${min}:${sec} ${merd}`
+}
+
+
     return(
         <>
-        <div className="clocl-box">
+        <div className="blurbg"></div>
+        <div className="clock-box">
             <div className="clock">
-                <span>00 : 00 : 00</span>
+                <span>{formattime()}</span>
             </div>
         </div>
         </>
